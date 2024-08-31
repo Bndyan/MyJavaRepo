a0809bc26b727e0e098fce8555cc6e4634b0369e
Karwan Karasool Hasan Bndyan <k.bndyan@itbh.local>
2024-08-31 21:46:31 +0200

    30.08.2024

---
+++ Hunde.java
@@ -0,0 +1,22 @@
+package javaprogramm;
+
+/**
+ * Die Klasse Hunde
+ *
+ * @author k.bndyan
+ * @version 1.0
+ * @date 31.08.2024
+ */
+public class Hunde {
+    // Attributs
+    int alter;
+    String art;
+    String fellfarbe;
+    boolean kannPfoteGeben;
+
+
+    // Methoden
+    public void  bellen(){
+        System.out.println("Wuff Wuff");
+    }
+}
---
+++ OOPJava.java
@@ -0,0 +1,17 @@
+package javaprogramm;
+
+/**
+ * Die Klasse OOPJava
+ *
+ * @author k.bndyan
+ * @version 1.0
+ * @date 31.08.2024
+ */
+public class OOPJava {
+    public static void main(String[] args) {
+
+        Hunde dutcheHund = new Hunde();
+        dutcheHund.bellen();
+
+    }
+}
